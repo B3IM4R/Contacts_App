f0941bd6e83305b095388a168777c3248ce925ce
B3IM4R <beimar.mestizo@gmail.com>
2024-09-22 21:49:56 -0500

    Add Contact Class

---
+++ Contact.java
@@ -0,0 +1,28 @@
+public class Contact {
+    // Atributos
+    private String name;
+    private String phoneNumber;
+    private String email;
+    
+    // Construtor
+    public Contact(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    // Getters e Setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + name + "\nTeléfono: " + phoneNumber + "\nCorreo: " + email;
+    }
+}
